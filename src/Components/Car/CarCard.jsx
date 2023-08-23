50e74e8292856b130733a98bd67621891ba6224b
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-08-24 01:44:36 +0530

    new update

--- src/Components/Car/CarCard.jsx
+++ src/Components/Car/CarCard.jsx
@@ -1,35 +1,34 @@
 import React from 'react'
 
-function CarCard() {
+function CarCard({ data }) {
     return (
 
         <div className="col-sm-4">
             <div className="card">
-                <img src="https://imgur.com/edOjtEC.png" className="card-img-top" width="100%" />
+                <img src={data.img} className="card-img-top" alt='' style={{ width: "100 %" }} />
                 <div className="card-body pt-0 px-0">
                     <div className="d-flex flex-row justify-content-between mb-0 px-3">
                         <small className="text-muted mt-1">STARTING AT</small>
-                        <h6>&dollar;22,495&ast;</h6>
+                        <h6>${data.price}*</h6>
                     </div>
                     <hr className="mt-2 mx-3" />
                     <div className="d-flex flex-row justify-content-between px-3 pb-4">
                         <div className="d-flex flex-column"><span className="text-muted">Fuel Efficiency</span><small
                             className="text-muted">L/100KM&ast;</small></div>
                         <div className="d-flex flex-column">
-                            <h5 className="mb-0">8.5/7.1</h5><small className="text-muted text-right">(city/Hwy)</small>
+                            <h5 className="mb-0">{data.fl.city}/{data.fl.highway}</h5><small className="text-muted text-right">(city/Hwy)</small>
                         </div>
                     </div>
                     <div className="d-flex flex-row justify-content-between p-3 mid">
                         <div className="d-flex flex-column"><small className="text-muted mb-1">ENGINE</small>
-                            <div className="d-flex flex-row"><img src="https://imgur.com/iPtsG7I.png" width="35px"
-                                height="25px" />
-                                <div className="d-flex flex-column ml-1"><small className="ghj">1.4L MultiAir</small><small
-                                    className="ghj">16V I-4 Turbo</small></div>
+                            <div className="d-flex flex-row"><img src="https://imgur.com/iPtsG7I.png" alt='' style={{ width: "35px", height: "25px" }} />
+                                <div className="d-flex flex-column ml-1"><small className="ghj">{data.engine.air}</small><small
+                                    className="ghj">{data.engine.cylinder}</small></div>
                             </div>
                         </div>
                         <div className="d-flex flex-column"><small className="text-muted mb-2">HORSEPOWER</small>
-                            <div className="d-flex flex-row"><img src="https://imgur.com/J11mEBq.png" />
-                                <h6 className="ml-1">135 hp&ast;</h6>
+                            <div className="d-flex flex-row"><img src="https://imgur.com/J11mEBq.png" alt='' />
+                                <h6 className="ml-1">{data.hp} hp*;</h6>
                             </div>
                         </div>
                     </div>
--- src/Components/Car/CarMain.jsx
+++ src/Components/Car/CarMain.jsx
@@ -1,31 +1,63 @@
-import React from 'react'
-import CarCard from './CarCard'
-import './assets/car.css'
+import React from 'react';
+import CarCard from './CarCard';
+import './assets/car.css';
 
 function CarMain() {
-
-    const CarData = [
-{ id: 1, price: '22495', fl: '8.5/7.1', engine: '1.4L MultiAir/16V I-4 Turbo', hp: '135' },
-{ id: 2, price: '27495', fl: '8.3/7.1', engine: '1.5L MultiAir/16V I-4 Turbo', hp: '135' },
-{ id: 3, price: '29495', fl: '8.2/7.1', engine: '1.6L MultiAir/16V I-4 Turbo', hp: '135' }
-    ]
-
-
-
-
-
-
+    const Cars = [
+        {
+            id: 1,
+            price: '22495',
+            fl: {
+                city: 8.3,
+                highway: 7.1,
+            },
+            engine: {
+                air: '1.5L MultiAir',
+                cylinder: '16V I-4 Turbo',
+            },
+            hp: '135',
+            img: 'https://imgur.com/edOjtEC.png',
+        },
+        {
+            id: 2,
+            price: '27495',
+            fl: {
+                city: 8.7,
+                highway: 8.1,
+            },
+            engine: {
+                air: '1.7L MultiAir',
+                cylinder: '16V I-4 Turbo',
+            },
+            hp: '165',
+            img: 'https://imgur.com/SKZolYE.png',
+        },
+        {
+            id: 3,
+            price: '29495',
+            fl: {
+                city: 8.9,
+                highway: 8.4,
+            },
+            engine: {
+                air: '1.9L MultiAir',
+                cylinder: '16V I-4 Turbo',
+            },
+            hp: '185',
+            img: 'https://imgur.com/gUQNKPd.png',
+        },
+    ];
     return (
         <>
             <div className="container-fluid d-flex justify-content-center">
                 <div className="row mt-5">
-                    <CarCard />
-                    <CarCard />
-                    <CarCard />
+                    {Cars.map((car) => (
+                        <CarCard key={car.id} data={car} />
+                    ))}
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default CarMain
+export default CarMain;
