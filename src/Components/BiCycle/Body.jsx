01a8a24ff7890c69a6b610101b2bd9ac6d957744
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-08-23 15:38:50 +0530

    new updfate

--- src/Components/BiCycle/Body.jsx
+++ src/Components/BiCycle/Body.jsx
@@ -3,10 +3,21 @@ import Img from './Img'
 import Info from './Info'
 
 function Body() {
+    const myinfo = [
+        '100% Quality',
+        'Free Shipping',
+        'Easy Returns',
+        '12 Months Warranty',
+        'EMI Starting from (On Credit Cards)',
+        'Normal Delivery : 4-5 Days',
+        'Express Delivery : 2-3 Days',
+        'COD Available (All Over India)'
+
+    ]
     return (
         <div className="container-body mt-4">
             <div className="row r3">
-                <Info />
+                <Info cyclelist={myinfo} />
                 <Img />
             </div>
         </div>
--- src/Components/BiCycle/Info.jsx
+++ src/Components/BiCycle/Info.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 
-function Info() {
+function Info(props) {
+
+    const mylist = props.cyclelist;
+    console.log('mylist:', mylist)
+    function ListItem() {
+        return (
+            mylist.map((e, i) => { return (<li key={i}>{e}</li>) })
+        );
+    }
+
+
+
+
+
     return (
         <div className="col-md-5 p-0 klo">
             <ul>
-                <li>100% Quality</li>
+                {ListItem()}
+                {/* <li>100% Quality</li>
                 <li>Free Shipping</li>
                 <li>Easy Returns</li>
                 <li>12 Months Warranty</li>
                 <li>EMI Starting from (On Credit Cards)</li>
                 <li>Normal Delivery : 4-5 Days</li>
                 <li>Express Delivery : 2-3 Days</li>
-                <li>COD Available (All Over India)</li>
+                <li>COD Available (All Over India)</li> */}
             </ul>
         </div>
     )
