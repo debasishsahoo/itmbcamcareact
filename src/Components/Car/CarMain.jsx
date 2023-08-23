21a4b5c2e96e4aea6cc820fcb3179e1022def600
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-08-23 16:21:11 +0530

    new update

--- src/Components/Car/CarMain.jsx
+++ src/Components/Car/CarMain.jsx
@@ -3,6 +3,18 @@ import CarCard from './CarCard'
 import './assets/car.css'
 
 function CarMain() {
+
+    const CarData = [
+{ id: 1, price: '22495', fl: '8.5/7.1', engine: '1.4L MultiAir/16V I-4 Turbo', hp: '135' },
+{ id: 2, price: '27495', fl: '8.3/7.1', engine: '1.5L MultiAir/16V I-4 Turbo', hp: '135' },
+{ id: 3, price: '29495', fl: '8.2/7.1', engine: '1.6L MultiAir/16V I-4 Turbo', hp: '135' }
+    ]
+
+
+
+
+
+
     return (
         <>
             <div className="container-fluid d-flex justify-content-center">
@@ -10,7 +22,6 @@ function CarMain() {
                     <CarCard />
                     <CarCard />
                     <CarCard />
-
                 </div>
             </div>
         </>
